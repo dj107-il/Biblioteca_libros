b51909109b9fb34c3c864722e2cbdc93837f1bc5
dj107-il <diegoballes4@gmail.com>
2026-03-15 20:28:46 -0500

    feat: añadiendo menú de búsqueda y reportes con todas sus funciones

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace Biblioteca_libros;
 
@@ -1023,6 +1025,316 @@ class Program
         Console.ReadLine();
     }
     
-    static void MostrarMenuBusquedaReportes(){}
+    static void MostrarMenuBusquedaReportes()
+    {
+        int menu_busqueda_reportes;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("===============================");
+            Console.WriteLine("  Menú de búsqueda y reportes ");
+            Console.WriteLine("===============================");
+            Console.WriteLine("1. Buscar libro.");
+            Console.WriteLine("2. Buscar usuario.");
+            Console.WriteLine("3. Reportes.");
+            Console.WriteLine("4. Volver al menú principal.");
+            Console.Write("Elija una de las opciones a la que desea ingresar: ");
+            menu_busqueda_reportes = int.Parse(Console.ReadLine() ?? "0");
+
+            switch (menu_busqueda_reportes)
+            {
+                case 1:
+                    //Funcion para buscar un libro
+                    BuscarLibro();
+                    break;
+
+                case 2:
+                    //Funcion para buscar un usuario
+                    BuscarUsuario();
+                    break;
+
+                case 3:
+                    //Funcion para hacer reportes sobre préstamos
+                    MenuReportes();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Volviendo al menú principal...");
+                    Console.Write("Presiona Enter para continuar... ");
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Opción invalida. Intentalo nuevamente");
+                    Console.Write("\nPresiona Enter para continuar...");
+                    Console.ReadLine();
+                    break;
+            }
+        } while (menu_busqueda_reportes != 4);   
+    }
+
+    static void BuscarLibro()
+    {
+        Console.Clear();
+        Console.WriteLine("Señor usuario por donde desea buscar el libro");
+        Console.WriteLine("¿por título o autor o ID/ISBN o por categoría?");
+        string respuesta_busqueda = Console.ReadLine() ?? "";
+
+        if(respuesta_busqueda.ToLower() == "titulo")
+        {
+            Console.Clear();
+            Console.WriteLine("¿Como se llama el libro?");
+            string titulo_libro = Console.ReadLine() ?? "";
+
+            Console.Clear();
+            Console.WriteLine($"Buscando libro por el título: {titulo_libro}");
+            Console.WriteLine("EL libro fue encontrado exitosamente. ✅");
+            Console.WriteLine("=======================================");                
+            Console.WriteLine("           ID/ISBN: 978-3-16           ");
+            Console.WriteLine($"        Título: {titulo_libro}        ");
+            Console.WriteLine("          Autor: Gabriel García        ");
+            Console.WriteLine("            Categoría: Novela          ");
+            Console.WriteLine("                Año: 2000              ");
+            Console.WriteLine("              Disponible: Si           ");
+            Console.WriteLine("=======================================");
+        }
+        else if(respuesta_busqueda.ToLower() == "autor")
+        {
+            Console.Clear();
+            Console.WriteLine("¿Como se llama el autor del libro?");
+            string autor_libro = Console.ReadLine() ?? "*";
+
+            Console.Clear();
+            Console.WriteLine($"Buscando libro por el autor: {autor_libro}");
+            Console.WriteLine("Los libros fueron encontrados exitosamente. ✅");
+            Console.WriteLine("\n==================================================================================================");
+            Console.WriteLine("  ID               Título              Autor     Categoría          Año   Disponible ");
+            Console.WriteLine($"977-2-12       Las maravillas          {autor_libro}     Fantasia           1988     si     ");
+            Console.WriteLine($"998-8-21       Los tolerantes          {autor_libro}     Suspenso           1975     no     ");
+            Console.WriteLine($"912-7-98     Wilson y sus cagadas      {autor_libro}     Drama, Comedia     1990     si     ");
+            Console.WriteLine($"\nSe listaron todos los libro populares que ha tenido el autor {autor_libro}.");
+            Console.WriteLine("==================================================================================================");
+        }
+        else if(respuesta_busqueda.ToLower() == "id/isbn" || respuesta_busqueda.ToLower() == "id")
+        {
+            Console.Clear();
+            Console.WriteLine("¿Cuál es el ID/ISBN del libro?");
+            string id_libro = Console.ReadLine() ?? "0";
+
+            Console.Clear();
+            Console.WriteLine($"Buscando libro por el ID/ISBN: {id_libro}");
+            Console.WriteLine("EL libro fue encontrado exitosamente. ✅");
+            Console.WriteLine("=======================================");                
+            Console.WriteLine($"              ID/ISBN: {id_libro}     ");
+            Console.WriteLine("            Título: Los sureños        ");
+            Console.WriteLine("           Autor: Esneider García      ");
+            Console.WriteLine("            Categoría: Historia        ");
+            Console.WriteLine("                Año: 2002              ");
+            Console.WriteLine("              Disponible: Si           ");
+            Console.WriteLine("=======================================");
+        }
+        else if(respuesta_busqueda.ToLower() == "categoria")
+        {
+            Console.Clear();
+            Console.WriteLine("¿Cuál es la categoría del libro?");
+            string categoria_libro = Console.ReadLine() ?? "";
+            Console.Clear();
+            Console.WriteLine($"Buscando libro por la categoría: {categoria_libro}");
+            Console.WriteLine("EL libro fue encontrado exitosamente. ✅");
+            Console.WriteLine("=========================================");                
+            Console.WriteLine("           ID/ISBN: 978-3-16           ");
+            Console.WriteLine("       Título: El cerebro de wilson    ");
+            Console.WriteLine("          Autor: Miguel Angel G        ");
+            Console.WriteLine($"           Categoría: {categoria_libro}");
+            Console.WriteLine("               Año: 2024               ");
+            Console.WriteLine("             Disponible: Si            ");
+            Console.WriteLine("=========================================");
+        }
+        else
+        {
+            Console.WriteLine("Has ingresado una respuesta invalida. Intentalo nuevamente.");
+        }
+
+        Console.Write("presiona Enter para continuar... ");
+        Console.ReadLine();
+    }
+
+    static void BuscarUsuario()
+
+    {
+        Console.Clear();
+        Console.WriteLine("Señor usuario por donde desea buscar el usuario");
+        Console.WriteLine("¿por nombre o id/documento?");
+        string respuesta_busqueda = Console.ReadLine() ?? "";
+
+        if(respuesta_busqueda.ToLower() == "nombre")
+        {
+            Console.Clear();
+            Console.WriteLine("¿Cuál es el nombre del usuario que estas buscando?");
+            string nombre_usuario = Console.ReadLine() ?? "Vacio";
+
+            Console.Clear();
+            Console.WriteLine($"Buscando al usuario por el nombre: {nombre_usuario}... ");
+            Console.WriteLine("El usuario ha sido encontrado exitosamente. ✅");
+            Console.WriteLine("========================================");
+            Console.WriteLine("        ID/Documento: 1013245235        ");
+            Console.WriteLine($"             Nombre: {nombre_usuario}      ");
+            Console.WriteLine("          Apellido: Restrepo            ");
+            Console.WriteLine("          Telefono: 3012414626          ");
+            Console.WriteLine($"    Correo electrónico:{nombre_usuario[0]}R10@gmail.com");
+            Console.WriteLine("           Estado: Activo");
+            Console.WriteLine("========================================");
+        }
+        else if(respuesta_busqueda.ToLower() == "id/documento" || respuesta_busqueda.ToLower() == "id")
+        {
+            Console.Clear();
+            Console.WriteLine("¿Cuál es el ID/ISBN del usuario que estas buscando?");
+            string id_usuario = Console.ReadLine() ?? "0";
+
+            Console.Clear();
+            Console.WriteLine($"Buscando al usuario por el id/documento: {id_usuario}... ");
+            Console.WriteLine("El usuario ha sido encontrado exitosamente. ✅");
+            Console.WriteLine("===============================================");
+            Console.WriteLine($"            ID/Documento: {id_usuario}           ");
+            Console.WriteLine("                Nombre: David                 ");
+            Console.WriteLine("               Apellido: Cardona               ");
+            Console.WriteLine("             Telefono: 3041268672              ");
+            Console.WriteLine($"    Correo electrónico: DCardona10@gmail.com  ");
+            Console.WriteLine("                Estado: Activo                 ");
+            Console.WriteLine("===============================================");
+        }
+        else
+        {
+            Console.WriteLine("Has ingresado una respuesta invalida. Intentalo nuevamente.");
+        }
+
+        Console.Write("presiona Enter para continuar... ");
+        Console.ReadLine();
+    }
+
+    static void MenuReportes()
+    {
+        int opciones_reportes;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("=========================");
+            Console.WriteLine("     Menú de reportes    ");
+            Console.WriteLine("=========================");
+            Console.WriteLine("1. Préstamo por usuario.");
+            Console.WriteLine("2. Préstamo por libro.");
+            Console.WriteLine("3. Préstamo vencidos.");
+            Console.WriteLine("4. Resumen general.");
+            Console.WriteLine("5. Volver al menú de búqueda y reportes.");
+            Console.Write("Señor usuario ingresa la opción que desea ingresar: ");
+            opciones_reportes = int.Parse(Console.ReadLine() ?? "0");
+
+            switch (opciones_reportes)
+            {
+                case 1:
+                    //Función para un reporte de un préstamo por usuario
+                    ReportePorUsuario();
+                    break;
+
+                case 2:
+                    //Función para un reporte de un préstamo por libro
+                    ReportePorLibro();
+                    break;
+                
+                case 3: 
+                    //Función para un reporte de un préstamo vencido(pasado la fecha limite)
+                    ReportePrestamoVencido();
+                    break;
+
+                case 4:    
+                    //Función para un reporte para dar un resumen general:total libros/disponibles y prestados
+                    ResumenGeneral();
+                    break;
+
+                case 5:
+                    Console.WriteLine("Volviendo al menu de búsqueda y reportes... ");
+                    Console.Write("Presiona Enter para continuar... ");
+                    Console.ReadLine();
+                    break;
+                
+                default:
+                    Console.WriteLine("Opción invalida. Intentalo nuevamente.");
+                    Console.Write("Presiona Enter para continuar... ");
+                    Console.ReadLine();
+                    break;
+            }
+        } while (opciones_reportes != 5);
+    }
+
+    static void ReportePorUsuario()
+    {
+        Console.Clear();
+        Console.WriteLine("=== Buscar reporte de préstamo por usuario ===");
+        Console.Write("Señor usuario ingrese el id/documento del usuario: ");
+        string id_usuario = Console.ReadLine() ?? "";
+
+        Console.Clear();
+        Console.WriteLine($"Buscando préstamos por usuario con el id/documento: {id_usuario}...");
+        Console.WriteLine($"Préstamos encontrados del usuario con el id/documento: {id_usuario} exitosamente.✅ \nCreando reporte....");
+        Console.WriteLine("=============================== Reporte por préstamo por usuario ========================================");
+        Console.WriteLine("ID Préstamo   ID Usuario    ID/ISBN Libro   Fecha Préstamo   Fecha Límite  Fecha Devolución   Estado   ");
+        Console.WriteLine($"  P-050         {id_usuario}       978-1-23        2024-01-10       2024-06-24         null          Activo   ");
+        Console.WriteLine($"  P-065         {id_usuario}       978-3-16        2024-02-05       2024-08-19      2024-08-10       Cerrado  ");
+        Console.WriteLine($"  P-080         {id_usuario}       978-4-56        2024-03-01       2024-11-15         null          Activo   ");
+        Console.WriteLine("==========================================================================================================");
+        Console.Write("\nPresiona Enter para continuar...");
+        Console.ReadLine();
+    }
+
+    static void ReportePorLibro()
+    {
+        Console.Clear();
+        Console.WriteLine("=== Buscar reporte de préstamo por libro ===");
+        Console.Write("Señor usuario ingrese el id/ISBN del libro: ");
+        string id_libro = Console.ReadLine() ?? "";
+
+        Console.Clear();
+        Console.WriteLine($"Buscando préstamos por libro con el ID/ISBN: {id_libro}...");
+        Console.WriteLine($"Préstamos encontrados del libro con el ID/ISBN: {id_libro} exitosamente.✅ \nCreando reporte....");
+        Console.WriteLine("=============================== Reporte por préstamo por libro ========================================");
+        Console.WriteLine("ID Préstamo   ID Usuario    ID/ISBN Libro   Fecha Préstamo   Fecha Límite  Fecha Devolución   Estado   ");
+        Console.WriteLine($"  P-050         101325686      {id_libro}        2024-01-10       2024-06-24         null          Activo   ");
+        Console.WriteLine($"  P-065         974-6-321      {id_libro}        2024-02-05       2024-08-19      2024-08-10       Cerrado  ");
+        Console.WriteLine($"  P-080         998-8-451      {id_libro}        2024-03-01       2024-11-15         null          Activo   ");
+        Console.WriteLine("==========================================================================================================");
+        Console.Write("\nPresiona Enter para continuar...");
+        Console.ReadLine();
+    }
+
+    static void ReportePrestamoVencido()
+    {
+        Console.Clear();
+        Console.WriteLine("==================== Reporte de préstamo vencidos ==============");
+        Console.WriteLine("ID Préstamo   ID Usuario   Fecha Límite   Días vencido   Estado");
+        Console.WriteLine("P-007         978-3-16     2024-01-24     45 días        Activo");
+        Console.WriteLine("P-015         916-5-61     2024-03-10     30 días        Activo");
+        Console.WriteLine("P-039         935-8-91     2024-05-18     15 días        Activo");
+        Console.WriteLine("================================================================");
+        Console.WriteLine("\n>>> Se listarían todos los préstamos vencidos del sistema.");
+        Console.Write("\nPresiona Enter para continuar...");
+        Console.ReadLine();
+    }
+
+    static void ResumenGeneral()
+    {
+        Console.Clear();
+        Console.WriteLine("===== Resumen general: total libros/disponibles/prestados =====");
+        Console.WriteLine("                  Total libros registrados: 10                 ");
+        Console.WriteLine("                     Libros disponibles: 7                     ");
+        Console.WriteLine("                      Libros prestados: 3                      ");
+        Console.WriteLine("                       Total usuarios: 15                      ");
+        Console.WriteLine("                      Usuarios activos: 12                     ");
+        Console.WriteLine("                       Total préstamos: 20                     ");
+        Console.WriteLine("                      Préstamos activos: 3                     ");
+        Console.WriteLine("                      Préstamos cerrados: 17                   ");
+        Console.WriteLine("===============================================================");
+        Console.Write("Presiona Enter para continuar... ");
+        Console.ReadLine();
+    }
     static void MostrarMenuGuardarCargarDatos(){}
 }
